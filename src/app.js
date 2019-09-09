bc6cff265a9f75f77a7360f8cccb3527f3519a35
ErandiCVC13 <hgftdrd@hotmail.com>
2019-09-09 12:51:13 -0500

    Se agrega nav mobile

--- src/app.js
+++ src/app.js
@@ -3,5 +3,16 @@ $("#menu-toggle").click(function (e) {
     $("#wrapper").toggleClass("toggled");
     $("#nav").toggleClass("fixed-top")
     $("#btn-nav").toggleClass("mt-6-1")
+    $("#wrapper").toggleClass("fixed-top");
+    $("#sidebar-wrapper").toggleClass("px-5");
+});
+
+$("#close").click(function (e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+    $("#nav").toggleClass("fixed-top")
+    $("#btn-nav").toggleClass("mt-6-1")
+    $("#wrapper").toggleClass("fixed-top");
+    $("#sidebar-wrapper").toggleClass("px-5");
 });
 
